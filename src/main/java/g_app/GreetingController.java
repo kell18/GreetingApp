f5551d5db5c65b3fb0b597e8e3bc03623c0c2b93
kell18 <albert.bikeev@gmail.com>
2016-11-10 19:05:34 +0300

    Make basic controllers

--- src/main/java/g_app/GreetingController.java
+++ src/main/java/g_app/GreetingController.java
@@ -2,16 +2,30 @@ package g_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/wellcome")
+    public String greetingPage(@CookieValue(value = "username", defaultValue = "") String username, Model model) {
+        if (username.isEmpty()) {
+            return "redirect:/login";
+        } else {
+            model.addAttribute("username", username);
+            return "wellcome";
+        }
     }
 
+    @PostMapping("/wellcome")
+    public String logout() {
+        return "redirect:/login";
+    }
 }
