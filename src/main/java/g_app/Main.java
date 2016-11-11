dca051b49f2f70d90eb465418366178203a98b51
kell18 <albert.bikeev@gmail.com>
2016-11-11 17:53:28 +0300

    Add .war file and fix minor templates errors

--- src/main/java/g_app/Main.java
+++ src/main/java/g_app/Main.java
@@ -2,11 +2,18 @@ package g_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
 
